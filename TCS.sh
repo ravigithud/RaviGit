7048c56d2069a1d5a14767f023b5b8818582401f
ravigithud <60174005+ravigithud@users.noreply.github.com>
2020-01-22 16:10:32 +0530

    Update TCS.sh

--- TCS.sh
+++ TCS.sh
@@ -1,6 +1,5 @@
 TCS welcomes you
 TCS means TATA Consutacy 
-
-
-
 if any openings are in TCS please let us know
+TCS is a MNC company and having 500 branches across world
+TCS is recruting freshers and aswell as exp candidates also
