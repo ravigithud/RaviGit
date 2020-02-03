a27578db9023febaf0bba84eacebe7341237e09e
ravigithud <60174005+ravigithud@users.noreply.github.com>
2020-02-03 16:07:35 +0530

    Update TCS.sh

--- TCS.sh
+++ TCS.sh
@@ -3,6 +3,6 @@ TCS means TATA Consutacy
 if any openings are in TCS please let us know
 TCS is a MNC company and having 500 branches across world
 TCS is recruting freshers and aswell as exp candidates also
-
-TCS is new branch atKolkalt"
+TCS is new branch at Kolkalta and Pune office
 TCS is started recruiting Devops Jr engg /Sr Devops engg /Desktop Engg
+TCS is now hiring Marketng people also
